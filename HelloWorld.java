f03b9ca8d561144ae877bb3d7e6123b68490907d
Rami <ahadramijr@gmail.com>
2022-10-03 19:51:36 -0400

    2nd commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Holly shit");
+
+        int x =10, y=15, z=15
+        if (x + y > 4) {
+            System.out.println("x + y is bigger than 4");
+        } else {
+            System.out.println(" x + y is NOT bigger than 4");
     }
 }
